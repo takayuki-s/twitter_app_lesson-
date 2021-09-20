f91472a229629332a78564bc3e854bd4b1ad941a
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-09-21 06:32:44 +0900

    型定義エラーの解消
    export const selectUser = (state: RootState) => state.user.user;
    
    export const store = configureStore({
      reducer: {
        user: userReducer,
      },
    });
    
    紐付け

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/useSlice";
+import userReducer from "../features/useSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    user: userReducer,
   },
 });
 
