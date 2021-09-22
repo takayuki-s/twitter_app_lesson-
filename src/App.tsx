38807c75ebfbb3ff9bd69f3125c160b2d015c8dc
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-09-22 22:01:55 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./App.module.css";
 
-function App() {
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser, login, logout } from "./features/useSlice";
+import { auth } from "./firebase";
+
+const App: React.FC = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const onSub = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      onSub();
+    };
+  }, [dispatch]);
+
   return <div className="App"></div>;
-}
+};
 
 export default App;
