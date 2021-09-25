0d2fcefd6d1d5b2dd8a05a8d272c0491d5a69d60
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-09-25 12:04:58 +0900

    画像の固定

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -28,7 +28,8 @@ const useStyles = makeStyles((theme) => ({
     height: "100vh",
   },
   image: {
-    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundImage:
+      "url(https://images.unsplash.com/photo-1468276311594-df7cb65d8df6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80)",
     backgroundRepeat: "no-repeat",
     backgroundColor:
       theme.palette.type === "light"
