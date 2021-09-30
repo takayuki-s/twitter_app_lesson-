d9f83f3a7e1856a16fbe93af340183d18e61aedb
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-10-01 06:53:45 +0900

    アバター画像表示&クリックでログアウト

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -1,7 +1,23 @@
+import { Avatar } from "@material-ui/core";
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/useSlice";
+import { auth } from "../firebase";
+import styles from "./TweetInput.module.css";
 
 const TweetInput = () => {
-  return <div>test</div>;
+  const user = useSelector(selectUser);
+  return (
+    <div>
+      <Avatar
+        className={styles.tweet_avatar}
+        src={user.photoUrl}
+        onClick={async () => {
+          await auth.signOut();
+        }}
+      />
+    </div>
+  );
 };
 
 export default TweetInput;
