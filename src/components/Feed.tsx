af2765cb5cd11e1cd4302187bec5481e70af1529
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-10-02 19:12:24 +0900

    tweet表示関数

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -4,6 +4,36 @@ import { db } from "../firebase";
 import TweetInput from "./TweetInput";
 
 const Feed: React.FC = () => {
+  const [posts, setPosts] = useState([
+    {
+      id: "",
+      avatar: "",
+      image: "",
+      text: "",
+      timestamp: null,
+      username: "",
+    },
+  ]);
+  useEffect(() => {
+    const unSub = db
+      .collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            avatar: doc.data().avatar,
+            image: doc.data().image,
+            text: doc.data().text,
+            timestamp: doc.data().timestamp,
+            username: doc.data().username,
+          }))
+        )
+      );
+    return () => {
+      unSub();
+    };
+  }, []);
   return (
     <div className={styles.feed}>
       <TweetInput />
