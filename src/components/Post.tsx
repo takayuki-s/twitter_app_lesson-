63676b8c04b43fa6d1f0615716d394eb801b150e
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-10-04 06:26:27 +0900

    Feedから受け取ったpropsをPostにて表示

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -19,7 +19,33 @@ interface PROPS {
 }
 
 const Post: React.FC<PROPS> = (props) => {
-  return <div></div>;
+  return (
+    <div className={styles.post}>
+      <div className={styles.post_avatar}>
+        <Avatar src={props.avatar} />
+      </div>
+      <div className={styles.post_body}>
+        <div>
+          <div className={styles.post_header}>
+            <h3>
+              <span className={styles.post_headerUser}>@{props.username}</span>
+              <span className={styles.post_headerTime}>
+                {new Date(props.timestamp?.toDate()).toLocaleString()}
+              </span>
+            </h3>
+          </div>
+          <div className={styles.post_tweet}>
+            <p>{props.text}</p>
+          </div>
+        </div>
+        {props.image && (
+          <div className={styles.post_tweetImage}>
+            <img src={props.image} alt="tweet" />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Post;
